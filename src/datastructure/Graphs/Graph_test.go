5bfb8691c986aae2b08cd58973433a81324a5e9b
Manish <manish.thakur@agoda.com>
2020-08-05 21:39:18 +0700

    insertion in a graph

---
+++ src/datastructure/Graphs/Graph_test.go
@@ -0,0 +1,33 @@
+package Graphs
+
+import (
+	"testing"
+)
+
+type Graph struct {
+	adjList map[string][]string
+}
+
+func (graph Graph) insert(u string, v string) {
+	graph.adjList[u] = append(graph.adjList[u], v)
+}
+
+func TestGraphInsertVertex(t *testing.T) {
+	graph := Graph{map[string][]string{}}
+	graph.insert("a", "b")
+	if len(graph.adjList) != 1 {
+		t.Errorf("Expected 1 vertex but found %d", len(graph.adjList))
+	}
+}
+
+func TestGraphAppendToExistingList(t *testing.T) {
+	graph := Graph{map[string][]string{}}
+	vertex := "a"
+	graph.insert(vertex, "b")
+	graph.insert(vertex, "c")
+	var expected [2]string
+	copy(expected[:], graph.adjList[vertex])
+	if expected != [2]string{"b", "c"} {
+		t.Errorf("Expected 1 vertex but found %d", len(graph.adjList))
+	}
+}
