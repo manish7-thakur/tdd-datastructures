de58d11b3570d94e588867a46d62f934b27e16d0
Manish <manish.thakur@agoda.com>
2020-08-08 12:37:44 +0700

    Test BFS with different vertices

--- src/datastructure/graphs/Graph_test.go
+++ src/datastructure/graphs/Graph_test.go
@@ -54,11 +54,29 @@ func TestGraphBFSMultipleVertices(t *testing.T) {
 	graph.insertAdj("d", []string{"b", "c", "e", "f"})
 	graph.insertAdj("e", []string{"d", "g"})
 	graph.insertAdj("f", []string{"d"})
-	graph.insertAdj("g", []string{"g"})
+	graph.insertAdj("g", []string{"e"})
 	var elems = graph.bfs("a")
 	var actual [7]string
 	copy(actual[:], elems)
 	expected := [7]string{"a", "b", "c", "d", "e", "f", "g"}
+	if actual != expected {
+		t.Errorf("Expected %v but found %v", expected, actual)
+	}
+}
+
+func TestGraphBFSMultipleVerticesLastNode(t *testing.T) {
+	graph := Graph{map[string][]string{}}
+	graph.insertAdj("a", []string{"b", "c"})
+	graph.insertAdj("b", []string{"a", "c", "d"})
+	graph.insertAdj("c", []string{"a", "b", "d"})
+	graph.insertAdj("d", []string{"b", "c", "e", "f"})
+	graph.insertAdj("e", []string{"d", "g"})
+	graph.insertAdj("f", []string{"d"})
+	graph.insertAdj("g", []string{"e"})
+	var elems = graph.bfs("g")
+	var actual [7]string
+	copy(actual[:], elems)
+	expected := [7]string{"g", "e", "d", "b", "c", "f", "a"}
 	if actual != expected {
 		t.Errorf("Expected %v but found %v", expected, actual)
 	}
