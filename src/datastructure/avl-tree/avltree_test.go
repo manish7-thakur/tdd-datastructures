bcadf28e617c6e55166ed92c59694a997d4430c6
Manish <manish.thakur@agoda.com>
2020-08-30 21:39:40 +0700

    avl tree with hhegits sum

--- src/datastructure/avl-tree/avltree_test.go
+++ src/datastructure/avl-tree/avltree_test.go
@@ -7,21 +7,40 @@ type AVlTree struct {
 }
 
 func (t *AVlTree) insert(value int) {
-	if t.root == nil {
-		t.root = &AvlNode{value, 0}
+	t.root = insert(t.root, value)
+}
+
+func insert(root *AvlNode, value int) *AvlNode {
+	if root == nil {
+		root = &AvlNode{value, 0, nil}
+	} else if value > root.value {
+		root.right = insert(root.right, value)
+		root.height = root.right.height + 1
 	}
+	return root
 }
 
 type AvlNode struct {
-	value int
-	bf    int
+	value  int
+	height int
+	right  *AvlNode
 }
 
 func TestInsertSingleNode(t *testing.T) {
 	tree := AVlTree{}
 	tree.insert(5)
 	root := tree.root
-	if root.bf != 0 {
-		t.Errorf("Expected balance factor to be %d but found %d", 0, root.bf)
+	if root.height != 0 {
+		t.Errorf("Expected balance factor to be %d but found %d", 0, root.height)
+	}
+}
+
+func TestInsertTwoNodes(t *testing.T) {
+	tree := AVlTree{}
+	tree.insert(5)
+	tree.insert(7)
+	root := tree.root
+	if root.height != 1 || root.right.height != 0 {
+		t.Errorf("Expected balance factor to be %d but found %d", -1, root.height)
 	}
 }
