7a6d9cb18e9ef430949d5a3130007150ebc80086
Manish <manish.thakur@agoda.com>
2020-09-02 21:07:04 +0700

    rotateRight

--- src/datastructure/avl-tree/avltree_test.go
+++ src/datastructure/avl-tree/avltree_test.go
@@ -1,6 +1,8 @@
 package avl_tree
 
-import "testing"
+import (
+	"testing"
+)
 
 type AVlTree struct {
 	root *AvlNode
@@ -10,17 +12,49 @@ func (t *AVlTree) insert(value int) {
 	t.root = insert(t.root, value)
 }
 
-func insert(root *AvlNode, value int) *AvlNode {
-	if root == nil {
-		root = &AvlNode{value, 0, nil, nil}
-	} else if value > root.value {
-		root.right = insert(root.right, value)
-		root.height = root.right.height + 1
+func insert(node *AvlNode, value int) *AvlNode {
+	if node == nil {
+		node = &AvlNode{value, 0, nil, nil}
+	} else if value > node.value {
+		node.right = insert(node.right, value)
 	} else {
-		root.left = insert(root.left, value)
-		root.height = root.left.height + 1
+		node.left = insert(node.left, value)
 	}
-	return root
+	node.height = Max(height(node.left), height(node.right)) + 1
+	bf := getBalanceFactor(node)
+	if bf == 2 {
+		node = rotateRight(node)
+	}
+	return node
+}
+
+func getBalanceFactor(node *AvlNode) int {
+	return height(node.left) - height(node.right)
+}
+
+func height(node *AvlNode) int {
+	if node != nil {
+		return node.height
+	}
+	return -1
+}
+
+func rotateRight(pivot *AvlNode) *AvlNode {
+	newTop := pivot.left
+	pivot.left = newTop.right
+	newTop.right = pivot
+
+	pivot.height = Max(height(pivot.left), height(pivot.right)) + 1
+	newTop.height = Max(height(newTop.left), height(newTop.right)) + 1
+
+	return newTop
+}
+
+func Max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
 }
 
 type AvlNode struct {
@@ -35,7 +69,7 @@ func TestInsertSingleNode(t *testing.T) {
 	tree.insert(5)
 	root := tree.root
 	if root.height != 0 {
-		t.Errorf("Expected balance factor to be %d but found %d", 0, root.height)
+		t.Errorf("Expected height to be %d but found %d", 0, root.height)
 	}
 }
 
@@ -45,7 +79,7 @@ func TestInsertTwoNodes(t *testing.T) {
 	tree.insert(7)
 	root := tree.root
 	if root.height != 1 || root.right.height != 0 {
-		t.Errorf("Expected balance factor to be %d but found %d", -1, root.height)
+		t.Errorf("Expected height to be %d but found %d", -1, root.height)
 	}
 }
 
@@ -56,6 +90,17 @@ func TestInsertThreeNodes(t *testing.T) {
 	tree.insert(4)
 	root := tree.root
 	if root.height != 1 || root.left.height != 0 {
-		t.Errorf("Expected balance factor to be %d but found %d", -1, root.height)
+		t.Errorf("Expected height to be %d but found %d", -1, root.height)
+	}
+}
+
+func TestInsertThreeNodesWithRotation(t *testing.T) {
+	tree := AVlTree{}
+	tree.insert(7)
+	tree.insert(5)
+	tree.insert(4)
+	root := tree.root
+	if root.height != 1 || root.left.height != 0 {
+		t.Errorf("Expected height to be %d but found %d", 1, root.height)
 	}
 }
