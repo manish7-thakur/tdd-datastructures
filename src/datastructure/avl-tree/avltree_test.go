22604e9f174f2111154165bbfd4a5846205bda73
Manish <manish.thakur@agoda.com>
2020-09-03 21:52:26 +0700

    use same data for test specs

--- src/datastructure/avl-tree/avltree_test.go
+++ src/datastructure/avl-tree/avltree_test.go
@@ -37,8 +37,8 @@ func TestInsertThreeNodes(t *testing.T) {
 func TestInsertThreeNodesWithRightRotation(t *testing.T) {
 	tree := AVlTree{}
 	tree.insert(7)
+	tree.insert(6)
 	tree.insert(5)
-	tree.insert(4)
 	root := tree.root
 	if root.height != 1 || root.left.height != 0 {
 		t.Errorf("Expected height to be %d but found %d", 1, root.height)
@@ -47,8 +47,8 @@ func TestInsertThreeNodesWithRightRotation(t *testing.T) {
 
 func TestInsertThreeNodesWithLeftRotation(t *testing.T) {
 	tree := AVlTree{}
-	tree.insert(4)
 	tree.insert(5)
+	tree.insert(6)
 	tree.insert(7)
 	root := tree.root
 	if root.height != 1 || root.left.height != 0 {
