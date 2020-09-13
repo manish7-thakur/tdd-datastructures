592fbe93c4136748f039434aaaf1256087c8ef62
Manish <manish.thakur@agoda.com>
2020-09-13 22:35:09 +0700

    remove commented code

--- src/datastructure/power/pow.go
+++ src/datastructure/power/pow.go
@@ -25,23 +25,6 @@ func (p Pow) calc(a int, n int) int {
 
 }
 
-/*func pow(a int, n int) int {
-	if n == 0 {
-		return 1
-	} else if n == 1 {
-		return a
-	} else {
-		res := mem[n]
-		if res == 0 {
-			nearest := nearestPow2(n)
-			rest := n - nearest
-			res = pow(a, nearest/2) * pow(a, nearest/2) * pow(a, rest)
-		}
-		mem[n] = res
-		return res
-	}
-}*/
-
 func nearestPow2(n int) int {
 	nearest := 2
 	for ; nearest*2 <= n; {
