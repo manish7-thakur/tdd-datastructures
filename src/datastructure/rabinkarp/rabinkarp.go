6b53a24f3cb49ec34555dcdb065751302743bbe3
Manish <manish.thakur@agoda.com>
2020-10-12 22:06:51 +0700

    store multiplicative in a variable

--- src/datastructure/rabinkarp/rabinkarp.go
+++ src/datastructure/rabinkarp/rabinkarp.go
@@ -4,16 +4,17 @@ import "math"
 
 func New(str string) HashGen {
 	priorHash := Hash(str)
-	return HashGen{priorHash, len(str)}
+	return HashGen{priorHash, len(str), math.Pow(31, float64(len(str)))}
 }
 
 type HashGen struct {
 	hash   int
 	strlen int
+	multiplicative float64
 }
 
 func (g *HashGen) RollHash(next rune, old rune) int {
-	g.hash = newHash(g.hash, next, old, float64(g.strlen))
+	g.hash = newHash(g.hash, next, old, int(g.multiplicative))
 	return g.hash
 }
 
@@ -25,8 +26,8 @@ func Hash(str string) int {
 	return int(hash)
 }
 
-func newHash(priorHash int, nextChar rune, oldChar rune, strlen float64) int {
-	hash := (31*priorHash + int(nextChar)) - (int(math.Pow(31, strlen)) * int(oldChar))
+func newHash(priorHash int, nextChar rune, oldChar rune, multiplicative int) int {
+	hash := (31*priorHash + int(nextChar)) - (multiplicative * int(oldChar))
 	return hash
 }
 
