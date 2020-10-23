40792914c59c7825618a2120d5effff1f3b24632
Manish <manish.thakur@agoda.com>
2020-10-23 12:10:11 +0700

    quick partition with upto 21 elms

---
+++ src/datastructure/quick-sort/quicksort_test.go
@@ -0,0 +1,44 @@
+package quick_sort
+
+import "testing"
+
+func TestPartitionEmptyArray(t *testing.T) {
+	res := partition([]int{})
+	if len(res) != 0 {
+		t.Errorf("expected empty but found %v", res)
+	}
+}
+
+func TestPartitionArrayOneElem(t *testing.T) {
+	res := partition([]int{2})
+	actual := [1]int{}
+	copy(actual[:], res)
+	expected := [1]int{2}
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, actual)
+	}
+}
+
+func TestPartitionArrayTwoElem(t *testing.T) {
+	res := partition([]int{2, 1})
+	actual := [2]int{}
+	copy(actual[:], res)
+	expected := [2]int{1, 2}
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, actual)
+	}
+}
+
+func partition(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] > arr[i+1] {
+			temp := arr[i]
+			arr[i] = arr[i+1]
+			arr[i+1] = temp
+		}
+	}
+	return arr
+}
