7bf02b24fa422da47076c0bf3e7d1e6f75e1e996
Manish <manish.thakur@agoda.com>
2020-11-07 17:15:43 +0700

    use multidimensional array instead of map

--- src/datastructure/tsp/tsp_test.go
+++ src/datastructure/tsp/tsp_test.go
@@ -6,20 +6,24 @@ import (
 )
 
 func TestSingleVertexShortestPath(t *testing.T) {
-	paths := make(map[string]float64, 1)
-	paths["AA"] = 0
-	res := tsp(paths, "A", "A", []string{"A"})
+	paths := make([][]float64, 1)
+	paths[0] = make([]float64, 1)
+	res := tsp(paths, 0, 0, []int{0})
 	if res != 0 {
 		t.Errorf("expected 0 but found %f", res)
 	}
 }
 
 func TestTwoVertexShortestPath(t *testing.T) {
-	paths := make(map[string]float64, 3)
-	paths["AA"] = 0
-	paths["AB"] = 2
-	paths["BA"] = 3
-	res := tsp(paths, "A", "A", []string{"B"})
+	paths := make([][]float64, 2)
+	for i := range paths {
+		paths[i] = make([]float64, 2)
+	}
+	paths[0][0] = 0
+	paths[0][1] = 2
+	paths[1][0] = 3
+	paths[1][1] = 0
+	res := tsp(paths, 0, 0, []int{0, 1})
 	expected := 5.0
 	if res != expected {
 		t.Errorf("expected %f but found %f", expected, res)
@@ -27,29 +31,32 @@ func TestTwoVertexShortestPath(t *testing.T) {
 }
 
 func TestThreVertexShortestPath(t *testing.T) {
-	paths := make(map[string]float64, 6)
-	paths["AA"] = 0
-	paths["AB"] = 2
-	paths["AC"] = 4
-	paths["BA"] = 3
-	paths["BB"] = 0
-	paths["BC"] = 4
-	paths["CA"] = 5
-	paths["CB"] = 3
-	paths["CC"] = 0
-	res := tsp(paths, "A", "A", []string{"B", "C"})
+	paths := make([][]float64, 3)
+	for i := range paths {
+		paths[i] = make([]float64, 3)
+	}
+	paths[0][0] = 0
+	paths[0][1] = 2
+	paths[0][2] = 4
+	paths[1][0] = 3
+	paths[1][1] = 0
+	paths[1][2] = 4
+	paths[2][0] = 5
+	paths[2][1] = 3
+	paths[2][2] = 0
+	res := tsp(paths, 0, 0, []int{1, 2})
 	expected := 10.0
 	if res != expected {
 		t.Errorf("expected %f but found %f", expected, res)
 	}
 }
 
-func tsp(costs map[string]float64, src string, curr string, vertices []string) float64 {
+func tsp(costs [][]float64, src int, curr int, vertices []int) float64 {
 	if len(vertices) == 1 {
-		return costs[curr+vertices[0]] + costs[vertices[0]+src]
+		return costs[curr][vertices[0]] + costs[vertices[0]][src]
 	} else {
-		v1 := costs[src+vertices[0]] + tsp(costs, src, vertices[0], vertices[1:])
-		v2 := costs[src+vertices[1]] + tsp(costs, src, vertices[1], vertices[:1])
+		v1 := costs[src][vertices[0]] + tsp(costs, src, vertices[0], vertices[1:])
+		v2 := costs[src][vertices[1]] + tsp(costs, src, vertices[1], vertices[:1])
 		return math.Min(v1, v2)
 	}
 }
