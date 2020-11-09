d81a879c8d8373b49fdec3bc363e2a0e0a81588c
Manish <manish.thakur@agoda.com>
2020-11-09 21:09:42 +0700

    SOS multi elements

--- src/datastructure/sos/sos_test.go
+++ src/datastructure/sos/sos_test.go
@@ -18,7 +18,7 @@ func TestSosZeroSum(t *testing.T) {
 	}
 }
 
-func TestSosOneElem(t *testing.T) {
+func TestSosOneElemSumPresent(t *testing.T) {
 	actual := sos(1, []int{1})
 	expected := true
 	if actual != expected {
@@ -26,9 +26,41 @@ func TestSosOneElem(t *testing.T) {
 	}
 }
 
-func TestSosTwoElem(t *testing.T) {
+func TestSosOneElemSumNotPresent(t *testing.T) {
+	actual := sos(1, []int{2})
+	expected := false
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, actual)
+	}
+}
+
+func TestSosTwoElemSumPresent(t *testing.T) {
 	actual := sos(3, []int{1, 2})
 	expected := true
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, actual)
+	}
+}
+
+func TestSosTwoElemSumNotPresent(t *testing.T) {
+	actual := sos(3, []int{1, 1})
+	expected := false
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, actual)
+	}
+}
+
+func TestSosMultiElemSumPresent(t *testing.T) {
+	actual := sos(87, []int{52, 41, 46, 9, 3, 12, 83, 32, 42, 85, 31, 6, 3, 55})
+	expected := true
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, actual)
+	}
+}
+
+func TestSosMultiElemNotPresent(t *testing.T) {
+	actual := sos(10, []int{52, 41, 46, 9, 3, 12, 83, 32, 42, 85, 31, 6, 3, 55})
+	expected := false
 	if actual != expected {
 		t.Errorf("expected %v but found %v", expected, actual)
 	}
