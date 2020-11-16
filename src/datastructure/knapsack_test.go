709ce3e26093080865f71138c149e74fef86e7a4
Manish <manish.thakur@agoda.com>
2020-11-16 22:01:02 +0700

    test if capapcity is zero

--- src/datastructure/knapsack_test.go
+++ src/datastructure/knapsack_test.go
@@ -4,7 +4,15 @@ import "testing"
 
 func TestKnapsackEmptyWeights(t *testing.T) {
 	capacity := 5
-	res := maxProfit([]int{}, []int{}, capacity, 0)
+	res := maxProfit([]int{}, []int{}, capacity)
+	if res != 0 {
+		t.Errorf("Expected empty but found %d", res)
+	}
+}
+
+func TestKnapsackWeightsZeroCapacity(t *testing.T) {
+	capacity := 0
+	res := maxProfit([]int{}, []int{}, capacity)
 	if res != 0 {
 		t.Errorf("Expected empty but found %d", res)
 	}
@@ -14,7 +22,7 @@ func TestKnapsackSingleWeight(t *testing.T) {
 	capacity := 5
 	weights := []int{5}
 	profits := []int{3}
-	res := maxProfit(weights, profits, capacity, 0)
+	res := maxProfit(weights, profits, capacity)
 	expected := 3
 	if res != expected {
 		t.Errorf("Expected %d but found %d", expected, res)
@@ -25,21 +33,21 @@ func TestKnapsackSingleWeightHigherThanCapacity(t *testing.T) {
 	capacity := 4
 	weights := []int{5}
 	profits := []int{3}
-	res := maxProfit(weights, profits, capacity, 0)
+	res := maxProfit(weights, profits, capacity)
 	expected := 0
 	if res != expected {
 		t.Errorf("Expected %d but found %d", expected, res)
 	}
 }
 
-func maxProfit(weights []int, profits []int, capacity int, currProfit int) int {
+func maxProfit(weights []int, profits []int, capacity int) int {
 	weightlen := len(weights)
-	if weightlen != 0 {
-		if weights[weightlen-1] > capacity {
-			maxProfit(weights[0:weightlen-1], profits[0:weightlen-1], capacity, currProfit)
-		} else {
-			currProfit += maxProfit(weights[0:weightlen-1], profits[0:weightlen-1], capacity-weights[weightlen-1], profits[weightlen-1])
-		}
+	if weightlen == 0 || capacity == 0 {
+		return 0
 	}
-	return currProfit
+	if weights[weightlen-1] > capacity {
+		return maxProfit(weights[0:weightlen-1], profits[0:weightlen-1], capacity)
+	} else {
+		return profits[weightlen-1] + maxProfit(weights[0:weightlen-1], profits[0:weightlen-1], capacity-weights[weightlen-1])
+	}
 }
