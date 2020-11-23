19901249cb843e8b4490cc51c604a49485a9a2c8
Manish <manish.thakur@agoda.com>
2020-11-23 22:31:57 +0700

    move code to prod file

---
+++ src/datastructure/max-contigous-subarray-sum/max_contigous_subarray_sum.go
@@ -0,0 +1,41 @@
+package max_contigous_subarray_sum
+
+import (
+	math2 "datastructure/math"
+	"math"
+)
+
+func MaxContiguousSubarraySum(arr []int) int {
+	arrlen := len(arr)
+	if arrlen == 0 {
+		return 0
+	}
+	if arrlen == 1 {
+		return arr[0]
+	}
+	mid := arrlen / 2
+	max1 := MaxContiguousSubarraySum(arr[:mid])
+	max2 := MaxContiguousSubarraySum(arr[mid+1:])
+	max3 := crossSum(arr, 0, mid, arrlen-1)
+	return math2.Max(math2.Max(max1, max2), max3)
+}
+
+func crossSum(arr []int, low, mid, high int) int {
+	sum := 0
+	leftSum := math.MinInt32
+	rightSum := math.MinInt32
+	for i := mid; i >= low; i-- {
+		sum += arr[i]
+		if sum > leftSum {
+			leftSum = sum
+		}
+	}
+	sum = 0
+	for i := mid + 1; i <= high; i++ {
+		sum += arr[i]
+		if sum > rightSum {
+			rightSum = sum
+		}
+	}
+	return math2.Max(math2.Max(leftSum, rightSum), leftSum+rightSum)
+}
--- src/datastructure/max-contigous-subarray-sum/max_subarray_sum_test.go
+++ src/datastructure/max-contigous-subarray-sum/max_subarray_sum_test.go
@@ -1,8 +1,6 @@
 package max_contigous_subarray_sum
 
 import (
-	"datastructure/math"
-	math2 "math"
 	"testing"
 )
 
@@ -60,39 +58,4 @@ func TestMaxContiguousSubarraySumMultiElementsNegative(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Expected %d but found %v", expected, actual)
 	}
-}
-
-func MaxContiguousSubarraySum(arr []int) int {
-	arrlen := len(arr)
-	if arrlen == 0 {
-		return 0
-	}
-	if arrlen == 1 {
-		return arr[0]
-	}
-	mid := arrlen / 2
-	max1 := MaxContiguousSubarraySum(arr[:mid])
-	max2 := MaxContiguousSubarraySum(arr[mid+1:])
-	max3 := crossSum(arr, 0, mid, arrlen-1)
-	return math.Max(math.Max(max1, max2), max3)
-}
-
-func crossSum(arr []int, low, mid, high int) int {
-	sum := 0
-	leftSum := math2.MinInt32
-	rightSum := math2.MinInt32
-	for i := mid; i >= low; i-- {
-		sum += arr[i]
-		if sum > leftSum {
-			leftSum = sum
-		}
-	}
-	sum = 0
-	for i := mid + 1; i <= high; i++ {
-		sum += arr[i]
-		if sum > rightSum {
-			rightSum = sum
-		}
-	}
-	return math.Max(math.Max(leftSum, rightSum), leftSum+rightSum)
 }
