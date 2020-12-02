7d91bc158a6dec5f26dc974f8e5538af7fe00e56
Manish <manish.thakur@agoda.com>
2020-12-02 21:44:26 +0700

    selection sort for multiple elems

--- src/datastructure/selection-sort/selectionsort_test.go
+++ src/datastructure/selection-sort/selectionsort_test.go
@@ -53,7 +53,7 @@ func TestSortMultipleElemArray(t *testing.T) {
 	res := SelectionSort([]int{24, 42, 73, 21, 37, 66, 10, 35, 46, 29, 45, 45, 23, 69, 34})
 	actual := [15]int{}
 	copy(actual[:], res)
-	expected := [15]int{1, 2, 3, 4}
+	expected := [15]int{10, 21, 23, 24, 29, 34, 35, 37, 42, 45, 45, 46, 66, 69, 73}
 	if actual != expected {
 		t.Errorf("expected %v but found %v", expected, actual)
 	}
