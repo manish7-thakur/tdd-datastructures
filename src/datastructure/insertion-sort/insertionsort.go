7e61116a6242751958a4e40a691c93e2c7de36c6
Manish <manish.thakur@agoda.com>
2020-12-02 21:06:57 +0700

    put the condition at loop level to stop further iteration

--- src/datastructure/insertion-sort/insertionsort.go
+++ src/datastructure/insertion-sort/insertionsort.go
@@ -2,10 +2,8 @@ package insertion_sort
 
 func InsertionSort(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
-		for j := i; j > 0; j-- {
-			if arr[j] < arr[j-1] {
-				arr[j], arr[j-1] = arr[j-1], arr[j]
-			}
+		for j := i; j > 0 && arr[j] < arr[j-1]; j-- {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
 		}
 	}
 	return arr
