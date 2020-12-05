422e60a6d67717aadad2a277551ffae37b9b5c2e
Manish <manish.thakur@agoda.com>
2020-12-05 21:12:06 +0700

    comb sort with multiple elems

--- src/datastructure/comb-sort/combsort_test.go
+++ src/datastructure/comb-sort/combsort_test.go
@@ -30,21 +30,59 @@ func TestSortingDoubleElemArray(t *testing.T) {
 }
 
 func TestSortingTripleElemArray(t *testing.T) {
-	res := CombSort([]int{2, 1})
-	actual := [2]int{}
+	res := CombSort([]int{3, 2, 1})
+	actual := [3]int{}
 	copy(actual[:], res)
-	expected := [2]int{1, 2}
+	expected := [3]int{1, 2, 3}
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, res)
+	}
+}
+
+func TestSortingFourElemArray(t *testing.T) {
+	res := CombSort([]int{4, 3, 2, 1})
+	actual := [4]int{}
+	copy(actual[:], res)
+	expected := [4]int{1, 2, 3, 4}
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, res)
+	}
+}
+
+func TestSortingFiveElemArray(t *testing.T) {
+	res := CombSort([]int{4, 3, 5, 2, 1})
+	actual := [5]int{}
+	copy(actual[:], res)
+	expected := [5]int{1, 2, 3, 4, 5}
+	if actual != expected {
+		t.Errorf("expected %v but found %v", expected, res)
+	}
+}
+
+func TestSortingMultiElemArray(t *testing.T) {
+	res := CombSort([]int{47, 33, 56, 22, 11, -26, 83, -4, 55, -25, -216, 75, 24, 69, 8})
+	actual := [15]int{}
+	copy(actual[:], res)
+	expected := [15]int{-216, -26, -25, -4, 8, 11, 22, 24, 33, 47, 55, 56, 69, 75, 83}
 	if actual != expected {
 		t.Errorf("expected %v but found %v", expected, res)
 	}
 }
 
 func CombSort(arr []int) []int {
-	if len(arr) <= 1 {
-		return arr
-	}
-	if arr[0] > arr[1] {
-		arr[0], arr[1] = arr[1], arr[0]
+	arrlen := len(arr)
+	width := arrlen / 2
+	for i := 0; i < arrlen; i++ {
+		for j := 0; j < arrlen-width; j++ {
+			next := j + width
+			if arr[j] > arr[next] {
+				arr[j], arr[next] = arr[next], arr[j]
+			}
+		}
+		width = width / 2
+		if width == 0 {
+			width = 1
+		}
 	}
 	return arr
 }
