a65c20c699f99cbe79d44d80887c2dd07c011b05
Manish <manish.thakur@agoda.com>
2020-12-05 22:24:33 +0700

    move code to src file

---
+++ src/datastructure/comb-sort/combsort.go
@@ -0,0 +1,29 @@
+package comb_sort
+
+func CombSort(arr []int) []int {
+	arrlen := len(arr)
+	width := nextGap(arrlen)
+	sorted := false
+	for ; !sorted; {
+		if width == 1 { // This is last iteration
+			sorted = true
+		}
+		for j := 0; j < arrlen-width; j++ {
+			next := j + width
+			if arr[j] > arr[next] {
+				arr[j], arr[next] = arr[next], arr[j]
+				sorted = false
+			}
+		}
+		width = nextGap(width)
+	}
+	return arr
+}
+
+func nextGap(width int) int {
+	gap := width * 10 / 13
+	if gap == 0 {
+		gap = 1
+	}
+	return gap
+}
--- src/datastructure/comb-sort/combsort_test.go
+++ src/datastructure/comb-sort/combsort_test.go
@@ -68,31 +68,3 @@ func TestSortingMultiElemArray(t *testing.T) {
 		t.Errorf("expected %v but found %v", expected, res)
 	}
 }
-
-func CombSort(arr []int) []int {
-	arrlen := len(arr)
-	width := nextGap(arrlen)
-	sorted := false
-	for ; !sorted; {
-		if width == 1 { // This is last iteration
-			sorted = true
-		}
-		for j := 0; j < arrlen-width; j++ {
-			next := j + width
-			if arr[j] > arr[next] {
-				arr[j], arr[next] = arr[next], arr[j]
-				sorted = false
-			}
-		}
-		width = nextGap(width)
-	}
-	return arr
-}
-
-func nextGap(width int) int {
-	gap := width * 10 / 13
-	if gap == 0 {
-		gap = 1
-	}
-	return gap
-}
