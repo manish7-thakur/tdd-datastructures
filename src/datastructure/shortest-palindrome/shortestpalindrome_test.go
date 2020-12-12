8d2fd31ada5eea84401819167862ea8dfc5fdcf7
Manish <manish.thakur@agoda.com>
2020-12-12 15:59:11 +0700

    move code to src file

--- src/datastructure/shortest-palindrome/shortestpalindrome_test.go
+++ src/datastructure/shortest-palindrome/shortestpalindrome_test.go
@@ -1,7 +1,6 @@
 package shortest_palindrome
 
 import (
-	"strings"
 	"testing"
 )
 
@@ -156,36 +155,3 @@ func TestShortestPalindromeEightChars(t *testing.T) {
 	}
 }
 
-func ShortestPalindrome(s string) string {
-	strlen := len(s)
-	if strlen < 2 {
-		return s
-	}
-	mid := strlen / 2
-	i := 0
-	j := 0
-	if strlen%2 == 0 {
-		i = mid - 1
-		j = mid
-	} else {
-		i = mid
-		j = mid
-	}
-	for ; i >= 0 && j < strlen; {
-		if s[i] == s[j] {
-			i--
-			j++
-		} else {
-			for ; i != j && s[i] != s[j]; {
-				j--
-			}
-		}
-	}
-	var b strings.Builder
-	b.Grow(2*strlen - j)
-	for i = strlen - 1; i >= j; i-- {
-		b.WriteByte(s[i])
-	}
-	b.WriteString(s)
-	return b.String()
-}
