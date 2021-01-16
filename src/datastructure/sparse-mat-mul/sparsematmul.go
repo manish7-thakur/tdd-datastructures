545c5e4162ac44a574b8c62959d820f1130af8d1
Manish <manish.thakur@agoda.com>
2021-01-16 13:07:37 +0700

    format code

--- src/datastructure/sparse-mat-mul/sparsematmul.go
+++ src/datastructure/sparse-mat-mul/sparsematmul.go
@@ -5,8 +5,8 @@ func multiply(a [3][3]int, b [3][3]int) [3][3]int {
 	for i := 0; i < len(a); i++ {
 		for j := 0; j < len(a); j++ {
 			for k := 0; k < len(a); k++ {
-				if(a[i][k] != 0 && b[k][j] != 0) {
-					res[i][j] += a[i][k] * b[k][j];
+				if a[i][k] != 0 && b[k][j] != 0 {
+					res[i][j] += a[i][k] * b[k][j]
 				}
 			}
 		}
@@ -15,40 +15,39 @@ func multiply(a [3][3]int, b [3][3]int) [3][3]int {
 }
 
 func multiply2(A [3][3]int, B [3][3]int) [3][3]int {
-        res := [len(A)][len(B[0])]int{};
-        rowA := [len(A)]bool{};
-        colB := [len(B[0])]bool{};
+	res := [len(A)][len(B[0])]int{}
+	rowA := [len(A)]bool{}
+	colB := [len(B[0])]bool{}
 
-        for i := 0 ; i < len(A); i++ {
-            for j := 0 ; j < len(A[0]); j++ {
-                if(A[i][j] != 0){
-                    rowA[i] = true;
-                    break;
-                }
-            }
-        }
-        for j := 0 ; j < len(B[0]); j++ {
-            for i := 0 ; i < len(B); i++ {
-            	  if(B[i][j] != 0){
-                    colB[j] = true;
-                    break;
-                }
-            }
-        }
+	for i := 0; i < len(A); i++ {
+		for j := 0; j < len(A[0]); j++ {
+			if A[i][j] != 0 {
+				rowA[i] = true
+				break
+			}
+		}
+	}
+	for j := 0; j < len(B[0]); j++ {
+		for i := 0; i < len(B); i++ {
+			if B[i][j] != 0 {
+				colB[j] = true
+				break
+			}
+		}
+	}
+	for i := 0; i < len(A); i++ {
+		for k := 0; k < len(B[0]); k++ {
+			if !rowA[i] || !colB[k] {
+				res[i][k] = 0
+				continue
+			}
 
-        for i := 0 ; i < len(A); i++ {
-            for k := 0 ; k < len(B[0]); k++ {
-                if(!rowA[i] || !colB[k]){
-                    res[i][k] = 0;
-                    continue;
-                }
-
-                sum := 0;
-                for j := 0 ; j < len(A[0]); j++ {
-                    sum += A[i][j] * B[j][k];
-                }
-                res[i][k] = sum;
-            }
-        }
-        return res;
-    }
+			sum := 0
+			for j := 0; j < len(A[0]); j++ {
+				sum += A[i][j] * B[j][k]
+			}
+			res[i][k] = sum
+		}
+	}
+	return res
+}
--- src/datastructure/sparse-mat-mul/sparsematmul_test.go
+++ src/datastructure/sparse-mat-mul/sparsematmul_test.go
@@ -1,30 +1,30 @@
 package sparsematmul
 
 import (
-	"testing"
 	"math/rand"
+	"testing"
 	"time"
 )
 
 func TestMulOneCrossOneMat(t *testing.T) {
-	a := [3][3]int{{1, 0, 0},{1, 0, 0},{0, 2, 3}}
+	a := [3][3]int{{1, 0, 0}, {1, 0, 0}, {0, 2, 3}}
 	b := [3][3]int{{0, 0, 1}, {1, 0, 0}, {0, 1, 0}}
 
 	actual := multiply(a, b)
-	expected := [3][3]int{{0,0,1}, {0,0,1}, {2,3,0}}
+	expected := [3][3]int{{0, 0, 1}, {0, 0, 1}, {2, 3, 0}}
 	if actual != expected {
-		t.Errorf("expected %v but found %v", expected, actual);
+		t.Errorf("expected %v but found %v", expected, actual)
 	}
 	actual = multiply2(a, b)
 	if actual != expected {
-		t.Errorf("expected %v but found %v", expected, actual);
+		t.Errorf("expected %v but found %v", expected, actual)
 	}
 }
 
 func BenchmarkMultiply(b *testing.B) {
 	mat1 := randomSparseMat()
 	mat2 := randomSparseMat()
-	for i :=0; i < b.N ; i++ {
+	for i := 0; i < b.N; i++ {
 		multiply(mat1, mat2)
 	}
 }
@@ -32,20 +32,20 @@ func BenchmarkMultiply(b *testing.B) {
 func BenchmarkMultiply2(b *testing.B) {
 	mat1 := randomSparseMat()
 	mat2 := randomSparseMat()
-	for i :=0; i < b.N ; i++ {
+	for i := 0; i < b.N; i++ {
 		multiply2(mat1, mat2)
 	}
 }
 
 func randomSparseMat() [3][3]int {
 	rand.Seed(time.Now().UnixNano())
-	mat := [3][3]int{};
-	for i := 0; i< len(mat); i++ {
-		for j :=0; j < len(mat); j++ {
-			if(rand.Intn(2) == 0) {
-				mat[i][j] = rand.Intn(10);
+	mat := [3][3]int{}
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat); j++ {
+			if rand.Intn(2) == 0 {
+				mat[i][j] = rand.Intn(10)
 			}
 		}
 	}
-	return mat;
+	return mat
 }
